1eaba85d49b336a03d808ea24ae7a439afa282e5
Wilkor <wilkorguitarrista@msn.com>
2024-01-09 16:11:39 -0300

    melhorias
    Adicionando regra para não ficar fazendo download toda hora ao clicar no botão no front

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.358d800eafd8bfabdb7bcaaf32addc64.js"
+  "./precache-manifest.d189742ec77d5c1fa1522461f4996d95.js"
 );
 
 self.addEventListener('message', (event) => {
